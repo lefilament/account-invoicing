3e26d51321ef47466b6f467268d5b337b24ff613
kong <rattapong.chok@gmail.com>
2019-07-23 21:06:34 +0700

    [12.0][UPG] account_debitnote

--- account_debitnote/models/account_invoice.py
+++ account_debitnote/models/account_invoice.py
@@ -1,3 +1,6 @@
+# Copyright 2019 Ecosoft (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
@@ -8,6 +11,7 @@ class AccountInvoice(models.Model):
     debit_invoice_id = fields.Many2one(
         comodel_name='account.invoice',
         string='Debit Note',
+        help="Reference to the origin invoice that create this debit note",
     )
     debit_invoice_ids = fields.One2many(
         comodel_name='account.invoice',
